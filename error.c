8f5d0af949686e20eeaf2588159ed22770e7a6d5
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-19 23:48:04 +0300

    Updating C source, ASCII text file: error.c

--- error.c
+++ error.c
@@ -18,11 +18,11 @@ void string_err(int error_code, ...);
  */
 void err(int error_code, ...)
 {
-	va_list ag;
-	char *op;
-	int l_num;
+	va_list arg;
+	char *oper;
+	int last_num;
 
-	va_start(ag, error_code);
+	va_start(arg, error_code);
 	switch (error_code)
 	{
 	case 1:
@@ -30,18 +30,18 @@ void err(int error_code, ...)
 		break;
 	case 2:
 		fprintf(stderr, "Error: Can't open file %s\n",
-				va_arg(ag, char *));
+				va_arg(arg, char *));
 		break;
 	case 3:
-		l_num = va_arg(ag, int);
-		op = va_arg(ag, char *);
-		fprintf(stderr, "L%d: unknown instruction %s\n", l_num, op);
+		last_num = va_arg(arg, int);
+		oper = va_arg(arg, char *);
+		fprintf(stderr, "L%d: unknown instruction %s\n", last_num, oper);
 		break;
 	case 4:
 		fprintf(stderr, "Error: malloc failed\n");
 		break;
 	case 5:
-		fprintf(stderr, "L%d: usage: push integer\n", va_arg(ag, int));
+		fprintf(stderr, "L%d: usage: push integer\n", va_arg(arg, int));
 		break;
 	default:
 		break;
@@ -60,29 +60,29 @@ void err(int error_code, ...)
  */
 void more_err(int error_code, ...)
 {
-	va_list ag;
-	char *op;
-	int l_num;
+	va_list arg;
+	char *oper;
+	int last_num;
 
-	va_start(ag, error_code);
+	va_start(arg, error_code);
 	switch (error_code)
 	{
 	case 6:
 		fprintf(stderr, "L%d: can't pint, stack empty\n",
-				va_arg(ag, int));
+				va_arg(arg, int));
 		break;
 	case 7:
 		fprintf(stderr, "L%d: can't pop an empty stack\n",
-				va_arg(ag, int));
+				va_arg(arg, int));
 		break;
 	case 8:
-		l_num = va_arg(ag, unsigned int);
-		op = va_arg(ag, char *);
-		fprintf(stderr, "L%d: can't %s, stack too short\n", l_num, op);
+		last_num = va_arg(arg, unsigned int);
+		oper = va_arg(arg, char *);
+		fprintf(stderr, "L%d: can't %s, stack too short\n", last_num, oper);
 		break;
 	case 9:
 		fprintf(stderr, "L%d: division by zero\n",
-				va_arg(ag, unsigned int));
+				va_arg(arg, unsigned int));
 		break;
 	default:
 		break;
@@ -99,18 +99,18 @@ void more_err(int error_code, ...)
  */
 void string_err(int error_code, ...)
 {
-	va_list ag;
-	int l_num;
+	va_list arg;
+	int last_num;
 
-	va_start(ag, error_code);
-	l_num = va_arg(ag, int);
+	va_start(arg, error_code);
+	last_num = va_arg(arg, int);
 	switch (error_code)
 	{
 	case 10:
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", l_num);
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", last_num);
 		break;
 	case 11:
-		fprintf(stderr, "L%d: can't pchar, stack empty\n", l_num);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", last_num);
 		break;
 	default:
 		break;
