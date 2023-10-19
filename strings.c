16c875dc6914adcf2d5840ce855176046c5059de
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-19 23:48:21 +0300

    Updating C source, ASCII text file: strings.c

--- strings.c
+++ strings.c
@@ -10,15 +10,14 @@ void print_str(stack_t **, unsigned int);
  */
 void print_char(stack_t **stack, unsigned int line_number)
 {
-	int ascii;
+	int aski;
 
 	if (stack == NULL || *stack == NULL)
 		string_err(11, line_number);
-
-	ascii = (*stack)->n;
-	if (ascii < 0 || ascii > 127)
+	aski = (*stack)->n;
+	if (aski < 0 || aski > 127)
 		string_err(10, line_number);
-	printf("%c\n", ascii);
+	printf("%c\n", aski);
 }
 
 /**
@@ -28,23 +27,22 @@ void print_char(stack_t **stack, unsigned int line_number)
  */
 void print_str(stack_t **stack, __attribute__((unused)) unsigned int ln)
 {
-	int ascii;
-	stack_t *tmp;
+	int aski;
+	stack_t *temp;
 
 	if (stack == NULL || *stack == NULL)
 	{
 		printf("\n");
 		return;
 	}
-
-	tmp = *stack;
-	while (tmp != NULL)
+	temp = *stack;
+	while (temp != NULL)
 	{
-		ascii = tmp->n;
-		if (ascii <= 0 || ascii > 127)
+		aski = temp->n;
+		if (aski <= 0 || aski > 127)
 			break;
-		printf("%c", ascii);
-		tmp = tmp->next;
+		printf("%c", aski);
+		temp = temp->next;
 	}
 	printf("\n");
 }
