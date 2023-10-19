39f4dd7bdc9f3f362f993eb1d36b54842df09912
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-19 23:10:13 +0300

    Updating C source, ASCII text file: main.c

--- main.c
+++ main.c
@@ -1,4 +1,8 @@
 #include "monty.h"
+
+stack_t *create_node(int n);
+void free_nodes(void);
+
 stack_t *head = NULL;
 
 /**
@@ -54,30 +58,4 @@ void free_nodes(void)
 		head = head->next;
 		free(tmp);
 	}
-}
-
-
-/**
- * add_to_queue - Adds a node to the queue.
- * @new_node: Pointer to the new node.
- * @ln: line number of the opcode.
- */
-void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
-{
-	stack_t *tmp;
-
-	if (new_node == NULL || *new_node == NULL)
-		exit(EXIT_FAILURE);
-	if (head == NULL)
-	{
-		head = *new_node;
-		return;
-	}
-	tmp = head;
-	while (tmp->next != NULL)
-		tmp = tmp->next;
-
-	tmp->next = *new_node;
-	(*new_node)->prev = tmp;
-
 }
